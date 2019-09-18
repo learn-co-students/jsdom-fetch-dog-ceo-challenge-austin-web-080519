8801745c67a9145df872821cd85922cfeaee5e09
Jason <jas0nle@yahoo.com>
2019-09-18 11:59:41 -0500

    done with challenge two without sub breed

--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-console.log('%c HI', 'color: firebrick');
+    console.log('%c Challenge Two', 'color: firebrick');
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
-//body
-const dogContainer = document.getElementById("dog-image-container");
+    //body
+    const dogContainer = document.getElementById("dog-image-container");
+    const dogBreeds = document.getElementById("dog-breeds");
 
-// fetching images from imgUrl
-fetch(imgUrl)
-    .then(function(response) {
-        // parses the response as JSON
-        return response.json();
-    })
-    .then(function(imageUrls) {
-        /* 
-        Planning on using a for...of to iterate through all imageUrls
-        to display via img src tag
-            for(const ELEMENT of ARRAY)
-        */
-        const images = imageUrls.message;
+    // fetching images from imgUrl - Challenge One
+    fetch(imgUrl)
+        .then(function(response) {
+            // parses the response as JSON
+            return response.json();
+        })
+        .then(function(imagesObject) {
+            /* 
+            Planning on using a for...of to iterate through all imageUrls
+            to display via img src tag
+                for(const ELEMENT of ARRAY)
+            */
+            const images = imagesObject.message;
 
-        for(const imageUrl of images) { 
-            console.log(`%c ${imageUrl}`, 'color: blue');
-            const ptag = document.createElement("p");
-            ptag.innerHTML = `<img src=${imageUrl}>`;
-            dogContainer.appendChild(ptag);
-        }
-    });
+            for(const imageUrl of images) { 
+               // console.log(imageUrl);
+                const ptag = document.createElement("p");
+                ptag.innerHTML = `<img src=${imageUrl}>`;
+                dogContainer.appendChild(ptag);
+            }
+        });
         
+
+    // fetching list of dog breeds - Challenge Two    
+    fetch(breedUrl)
+        .then(function(response){
+            //  parsing response to json
+            return response.json();
+        })
+        .then(function(breedsObject){
+
+            const breeds = breedsObject.message            
+
+            // Use for...in to iterate over Object of breeds
+            // console.log(Object.keys(breeds));
+            for (const breed in breeds) {
+                console.log(breeds[breed]);
+                const li = document.createElement("li");
+                li.innerHTML = breed;
+                dogBreeds.appendChild(li);
+            };
+        })
 });
